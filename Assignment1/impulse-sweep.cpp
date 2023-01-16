71fd116e4529bc9dbd87635e2b78e5e4ecb17ea7
Jinjin He <hust_hjj@126.com>
2023-01-16 15:33:17 -0800

    impulse

--- Assignment1/impulse-sweep.cpp
+++ Assignment1/impulse-sweep.cpp
@@ -1,1 +1,19 @@
-int main(int argc, char* argv[]) { return 1; }
+#include "everything.h"
+
+int main(int argc, char* argv[]) {
+    float phase = 0;
+    double fourier = 0;
+
+    for (float note = 127; note > 0; note -= 0.001) {
+        float frequency = mtof(note);
+        int maxH = ((double)SAMPLE_RATE / 2.0f * 0.8f) / frequency;
+        for (int j = 0; j < maxH; j++) {
+            fourier += sin(j * phase);
+        }
+        fourier /= maxH;
+        mono(fourier * 0.707);
+        phase += 2 * pi * frequency / SAMPLE_RATE;
+        if (phase > 2 * pi)  //
+        phase -= 2 * pi;
+    }
+}
--- Assignment1/impulse.cpp
+++ Assignment1/impulse.cpp
@@ -1,1 +1,37 @@
-int main(int argc, char* argv[]) { return 1; }
+#include <string>
+
+#include "everything.h"  // mono
+
+int main(int argc, char* argv[]) {
+    double frequency = 440.0;
+
+    if (argc > 1) {
+        frequency = std::stof(argv[1]);
+    }
+
+    double duration = 1;
+    if (argc > 2) {
+        duration = std::stof(argv[2]);
+    }
+
+    double phase = 0;
+    double fourier = 0;
+    // below 80% of the Nyquist frequency
+    int maxH = (double)SAMPLE_RATE / 2.0f * 0.8f / frequency;
+    for (int i = 0; i < duration * SAMPLE_RATE; ++i) {
+        fourier = 0;
+        for (int j = 0; j < maxH; j++) {
+            fourier += sin(j * phase);
+        }
+        fourier /= maxH;
+        mono(fourier);
+
+        phase += 2 * pi * frequency / SAMPLE_RATE;
+
+        if (phase > 2 * pi) {
+        phase -= 2 * pi;
+        }
+    }
+
+    return 0;
+}
